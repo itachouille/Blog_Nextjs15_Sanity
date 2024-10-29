398032a602e49583a1ea66285f4e946c4f3ce77a
Anthony David <anthony.david34@sfr.fr>
2024-10-29 17:07:18 +0100

    connect google auth to Sanity to create user, session and token

--- auth.ts
+++ auth.ts
@@ -1,6 +1,51 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { client } from "./sanity/lib/client";
+import { AUTHOR_BY_GOOGLE_ID_QUERY } from "./sanity/lib/queries";
+import { writeClient } from "./sanity/lib/write-client";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [Google],
+  callbacks: {
+    async signIn({ user, account }) {
+      const googleId = account?.providerAccountId;
+
+      const { name, email, image } = user;
+
+      const existingUser = await client
+        .withConfig({ useCdn: false })
+        .fetch(AUTHOR_BY_GOOGLE_ID_QUERY, {
+          id: googleId,
+        });
+
+      if (!existingUser) {
+        await writeClient.create({
+          _type: "author",
+          id: googleId,
+          name,
+          email,
+          image,
+        });
+      }
+
+      return true;
+    },
+    async jwt({ token, account, profile }) {
+      if (account && profile) {
+        const user = await client
+          .withConfig({ useCdn: false })
+          .fetch(AUTHOR_BY_GOOGLE_ID_QUERY, {
+            id: account?.providerAccountId,
+          });
+
+        token.id = user?._id;
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      Object.assign(session, { id: token.id });
+      return session;
+    },
+  },
 });
